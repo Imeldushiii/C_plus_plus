df361f43f2464ef14d002f10065ec41d31a34455
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2023-08-07 16:08:48 +0200

    Add files via upload

---
+++ 2.Max()_highest_number_from_2_ints.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h> // BIBLIOTEKA OD TEGO
+
+
+int main()
+{
+    int result = std::max(5,70); //WYBIERA NAM Z 2 INTOW NAJWIEKSZA LICZBE
+    std::cout << result << std::endl;
+    return 0;
+}
