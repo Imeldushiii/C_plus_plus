cf67f862172bf8a81b0bf8019f8522ed1160a66f
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2023-08-07 16:11:48 +0200

    Add files via upload

---
+++ 3.Min()_Lowest_number_from_2_ints.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h> // BIBLIOTEKA OD TEGO
+//ALBO CMATH
+
+int main()
+{
+    int result = std::min(5,70); //WYBIERA NAM Z 2 INTOW NAJmniejsza LICZBE
+    std::cout << result << std::endl;
+    return 0;
+}
