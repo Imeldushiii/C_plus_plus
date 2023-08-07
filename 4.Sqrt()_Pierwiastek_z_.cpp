75828f16acb6843e6bd8fc468a0654883712eea2
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2023-08-07 16:20:53 +0200

    Add files via upload

---
+++ 4.Sqrt()_Pierwiastek_z_.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h> // BIBLIOTEKA OD TEGO
+//ALBO CMATH
+
+int main()
+{
+    int result = std::sqrt(100); //Pierwiastek
+    std::cout << result << std::endl;
+    return 0;
+}
