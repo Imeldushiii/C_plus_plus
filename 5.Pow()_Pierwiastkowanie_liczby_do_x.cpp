b19578154bb58f63a9b4bc82564dfa698148dd6d
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2023-08-07 16:30:11 +0200

    Add files via upload

---
+++ 5.Pow()_Pierwiastkowanie_liczby_do_x.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h> // BIBLIOTEKA OD TEGO
+//ALBO CMATH
+
+int main()
+{
+    int result = std::pow(2,10); //Potegowanie
+    // 2*2*2*2*2*2*2*2*2*2 = 1024
+    std::cout << result << std::endl;
+    return 0;
+}
